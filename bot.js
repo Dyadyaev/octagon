aed831ee3570271d80ac4c4f9b42386853763dbc
Dyadyaev <DED-113@ya.ru>
2026-06-12 21:57:08 +0300

    тг еще команды

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
+const db = require('./db');
 
 const token = '8873635811:AAE9pj87A36E7fifFNaEMPtmsMLWXtgQkmU';
 
@@ -20,7 +21,10 @@ bot.onText(/\/help/, (msg) => {
     bot.sendMessage(
         msg.chat.id,
         '/site - сайт Октагон\n' +
-        '/creator - информация о создателе'
+        '/creator - информация о создателе\n' +
+	'/deleteItem - удалить предмет\n' +
+	'/getItemByID - добавить\n' +
+	'/randomItem - рандом'
     );
 
 });
@@ -39,6 +43,92 @@ bot.onText(/\/creator/, (msg) => {
     bot.sendMessage(
         msg.chat.id,
         'Дядяев Ярослав'
+    );
+
+});
+
+bot.onText(/\/randomItem/, (msg) => {
+
+    db.query(
+        'SELECT * FROM items ORDER BY RAND() LIMIT 1',
+        (err, rows) => {
+
+            if (err || rows.length === 0) {
+                return bot.sendMessage(
+                    msg.chat.id,
+                    'Ошибка'
+                );
+            }
+
+            const item = rows[0];
+
+            bot.sendMessage(
+                msg.chat.id,
+                `(${item.id}) - ${item.name}: ${item.desc}`
+            );
+
+        }
+    );
+
+});
+
+bot.onText(/\/getItemByID (.+)/, (msg, match) => {
+
+    const id = Number(match[1]);
+
+    db.query(
+        'SELECT * FROM items WHERE id = ?',
+        [id],
+        (err, rows) => {
+
+            if (err || rows.length === 0) {
+                return bot.sendMessage(
+                    msg.chat.id,
+                    'Ошибка'
+                );
+            }
+
+            const item = rows[0];
+
+            bot.sendMessage(
+                msg.chat.id,
+                `(${item.id}) - ${item.name}: ${item.desc}`
+            );
+
+        }
+    );
+
+});
+
+bot.onText(/\/deleteItem (.+)/, (msg, match) => {
+
+    const id = Number(match[1]);
+
+    db.query(
+        'DELETE FROM items WHERE id = ?',
+        [id],
+        (err, result) => {
+
+            if (err) {
+                return bot.sendMessage(
+                    msg.chat.id,
+                    'Ошибка'
+                );
+            }
+
+            if (result.affectedRows === 0) {
+                return bot.sendMessage(
+                    msg.chat.id,
+                    'Ошибка'
+                );
+            }
+
+            bot.sendMessage(
+                msg.chat.id,
+                'Удачно'
+            );
+
+        }
     );
 
 });
